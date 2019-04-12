121b917ebbbf4e67d400b89673a3735b906c914c
Luc <luc@ltv.vn>
2019-04-12 15:18:54 +0700

    add gql-merge typings

---
+++ packages/gql-merge/index.d.ts
@@ -0,0 +1,36 @@
+declare module 'gql-merge' {
+  /**
+ * Find GraphQL files based on a glob pattern and merge the results.
+ * @param {string} fileGlob - A glob pattern to find files, e.g. '*.graphql'
+ * @return {Promise<string>} A promise of the resulting string.
+ */
+  export async function mergeFileGlob(fileGlob: string): Promise<string>;
+
+  /**
+   * Find GraphQL files based on a glob pattern and merge the results.
+   * @param {string} fileGlob - A glob pattern to find files, e.g. '*.graphql'
+   * @return {Promise<string>} A promise of the resulting string.
+   */
+  export async function mergeFilePaths(filePaths: string[]): Promise<string>;
+
+  /**
+   * Merges an array of GraphQL strings into one
+   * @param {string[]} schemaStrs - An array of GraphQL strings.
+   * @return {string} The resulting merged GraphQL string.
+   */
+  export function mergeStrings(schemaStrs: string[]): string;
+
+  /**
+   * Merges duplicate definitions in a single GraphQL string
+   * @param {string} schemaStr - The GraphQL String.
+   * @return {string} The resulting merged GraphQL string.
+   */
+  export function mergeString(schemaStr: string): string;
+
+  /**
+   * Merges duplicate definitions in a single GraphQL abstract-syntax tree
+   * @param {Document} schemaAst - The GraphQL AST.
+   * @return {string} The resulting merged GraphQL string.
+   */
+  export function mergeAst(schemaAst: Document): string;
+}
